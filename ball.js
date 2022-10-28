97e43760d2938084bb0bb541adeaababbaa1a2b4
Adrietis <114991766+Adrietis@users.noreply.github.com>
2022-10-28 10:15:05 +0200

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,11 @@
+
+export class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x; //posición horizontal
+    this.y = y; //posición vertical
+    this.velX = velX; //velocidad horizontal
+    this.velY = velY; //velocidad vertical
+    this.color = color; //color
+    this.size = size; //tamaño
+  }
+}
